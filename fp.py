60e26f8e8372a266df222da831c1757670ecb76d
Alastair Porter <alastair@porter.net.nz>
2010-04-25 22:14:38 +1200

    Move fingerprinting into separate file

---
+++ fp.py
@@ -0,0 +1,50 @@
+import os
+import subprocess
+import wave
+import tempfile
+import pycodegen
+import struct
+import json
+
+supported_types = [".mp3", ".ogg", ".flac"]
+
+
+def decode(file):
+	if os.path.splitext(file)[1] in supported_types:
+		(fd,outfile)=tempfile.mkstemp(suffix=".wav")
+		os.close(fd)
+		args = ["ffmpeg", "-y", "-i", file, "-ac", "1", "-ar", "22050", "-f", "wav", "-t", "20", "-ss", "10", outfile]
+		print "decoding",file,"..."
+		subprocess.call(args, stderr=open("/dev/null", "w"))
+		return outfile
+	else:
+		return None
+
+def fingerprint(file):
+	outfile = ""
+	MAGIC = 32768.0
+	try:
+		outfile = decode(file)
+		if outfile is not None:
+			wav = wave.open(outfile, "rb")
+			print "numframes:",wav.getnframes()
+
+			frames = wav.readframes(wav.getnframes())
+			fs = []
+			for i in range(0, len(frames), 2):
+				fs.append(struct.unpack("<H", frames[i:i+2])[0]/MAGIC)
+			
+			print "num samples", len(fs)
+			cg = pycodegen.pycodegen(fs, 10)
+			codes = cg.getCodes()
+			print "num codes", len(codes)
+			codestr = " ".join([str(x) for x in codes])
+			query = json.dumps({"codes": codestr})
+			return query
+
+	finally:
+		if outfile is not None and os.path.exists(outfile):
+			print "unlinking file"
+			os.unlink(outfile)
+
+
--- identify.py
+++ identify.py
@@ -10,48 +10,16 @@ import subprocess
 import tempfile
 import struct
 import json
+import fp
+import echonest
 
 supported_types = [".mp3", ".ogg", ".flac"]
 
-def decode(file):
-	if os.path.splitext(file)[1] in supported_types:
-		(fd,outfile)=tempfile.mkstemp(suffix=".wav")
-		os.close(fd)
-		args = ["ffmpeg", "-y", "-i", file, "-ac", "1", "-ar", "22050", "-f", "wav", "-t", "20", "-ss", "10", outfile]
-		print "decoding",file,"..."
-		subprocess.call(args, stderr=open("/dev/null", "w"))
-		return outfile
-	else:
-		return None
-
-def fingerprint(file):
-	outfile = ""
-	MAGIC = 32768.0
-	try:
-		outfile = decode(file)
-		wav = wave.open(outfile, "rb")
-		print "numframes:",wav.getnframes()
-
-		frames = wav.readframes(wav.getnframes())
-		fs = []
-		for i in range(0, len(frames), 2):
-			fs.append(struct.unpack("<H", frames[i:i+2])[0]/MAGIC)
-		
-		print "num samples", len(fs)
-		cg = pycodegen.pycodegen(fs, 10)
-		codes = cg.getCodes()
-		print "num codes", len(codes)
-		codestr = " ".join([str(x) for x in codes])
-		query = json.dumps({"codes": codestr})
-		print query
-
-	finally:
-		if os.path.exists(outfile):
-			os.unlink(outfile)
-
 def main(dir):
 	for f in os.listdir(dir):
-		fingerprint(os.path.join(dir, f))
+		print "file",f
+		query = fp.fingerprint(os.path.join(dir, f))
+		echonest.pp(echonest.fp_lookup(query))
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
