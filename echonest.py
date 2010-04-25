1d3de3e3d93916e66828709cc2b6d535cde5b7b3
Alastair Porter <alastair@porter.net.nz>
2010-04-25 22:14:15 +1200

    Initial cut of an echonest v4 api abstraction layer

---
+++ echonest.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import os
+import sys
+import urllib2
+import urlparse
+import urllib
+import echonestconf
+import json
+
+def _do_en_query(method, postdata=None, **kwargs):
+        args = {}
+        for k,v in kwargs.items():
+                args[k] = v.encode("utf8")
+	args["api_key"] = echonestconf.echonest_key
+	args["format"]="json"
+
+        url=urlparse.urlunparse(('http',
+                'beta.developer.echonest.com',
+                '/api/v4/%s' % method,
+                '',
+                urllib.urlencode(args),
+                ''))
+        print >> sys.stderr, "opening url",url
+        f = urllib2.Request(url)
+        try:
+                f = urllib2.urlopen(f)
+        except Exception, e:
+                print >> sys.stderr, e.msg
+                print >> sys.stderr, e.fp.read()
+                raise
+	return json.loads(f.read())
+
+def artist_profile(artistid):
+	return _do_en_query("artist/profile", bucket="id:musicbrainz", id=artistid)
+
+def fp_lookup(codes, artist="", track=""):
+	return _do_en_query("alpha_identify_song", postdata=codes)
+
+def pp(data):
+	print json.dumps(data, indent=4)
+
+def main():
+	pp(artist_profile("ARH6W4X1187B99274F"))
+
+if __name__ =="__main__":
+	if len(sys.argv) < 2:
+		print "usage: %s [mbid|] <args>" % sys.argv[0]
+		sys.exit()
+	else:
+		main()
